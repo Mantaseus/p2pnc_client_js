8c88368044a3936cc561423954109f3f06711a10
Satnam Singh Brar <brar.satnam1995@gmail.com>
2020-01-26 15:24:53 -0800

    Got the P2PNC connection working
    - Copied over most of the code from the commented out part that had been
    used for the older implementation. Had to adjust a few things to get it
    working
    - But it all just works with relatively few adjustments

--- cli.js
+++ cli.js
@@ -54,12 +54,6 @@ function printStats(conn) {
     });
 }
 
-function printSDP(sdp) {
-    const jsonObj = JSON.parse(String(sdp));
-    //const stringToPrint = `SDP ${jsonObj.type}: \n${jsonObj.sdp}`
-    console.log(`SDP ${jsonObj.type}: \n    ${jsonObj.sdp.replace(/\n/g, '\n    ')}`);
-}
-
 // MAIN -------------------------------------------------------------------------------------------
 
 let peerjsServerId = ''
@@ -87,6 +81,8 @@ const peer = new Peer({debug: 2});
 peer.on('open', () => {
     console.log('CONNECTED TO PEERJS SERVER');
 
+    let serverPortConnected = false;
+
     const conn = peer.connect(peerjsServerId);
 
     conn.on('open', () => {
@@ -105,14 +101,102 @@ peer.on('open', () => {
         });
 
         conn.on('data', (data) => {
-            console.log(data);
+            // We should only ever see any data in here as a response to the command that we send
+            // to the server to specify which server we want to create a tunnel for. So, we can
+            // safely ignore any subsequent data
+            if (serverPortConnected) return
+
+            data = '' + data;
+            if (data === 'ok'){
+                // The server tells us that it has connected to the port that the user requested and is
+                // ready to forward the p2p data stream to it
+                console.log(`Peer server listening on port ${args['<serverPort>']}`);
+
+                // Setup a TCP server for the client user to use
+                const tcpServer = new net.createServer((socket) => {
+                    if (args['--verbose']) {
+                        console.log(`Something connected to local port ${args['<localPort>']}`);
+                    }
+
+                    // Create a new data channel on the peer connection with a random name
+                    const newDataChannelName = 'custom_' + randomBytes(20).toString('hex');
+                    const dataChannel = conn.peerConnection.createDataChannel(newDataChannelName);
+                    dataChannel.binaryType = 'arraybuffer';
+                    dataChannel.onmessage = (event) => {
+                        const data = Buffer.from(event.data);
+                        socket.write(data);
+                    }
+                    dataChannel.onclose = () => {
+                        if (args['--verbose']) {
+                            console.log('data channel closed');
+                        }
+                    }
+                    dataChannel.onerror = (err) => {
+                        console.log('data channel error: ' + err);
+                    }
+
+                    // Setup the socket to the use the data from the p2p data channel
+                    socket.setKeepAlive(true);
+                    socket.on('data', (data) => {
+                        // Protect the data channel buffer from getting filled up
+                        if (dataChannel.bufferedAmount < MAX_DATA_CHANNEL_BUFFER_SIZE){
+                            try {
+                                dataChannel.send(data);
+                            } catch(e) {
+                                console.log(e);
+                                tcpClient.end();
+                            }
+                        } else {
+                            tcpClient.pause();
+
+                            // Check the bufferedAmount until it goes acceptably low
+                            const bufferCheckInterval = setInterval(() => {
+                                if (dataChannel.bufferedAmount === 0){
+                                    try {
+                                        dataChannel.send(data);
+                                    } catch(e) {
+                                        console.log(e);
+                                        tcpClient.end();
+                                    }
+                                    clearInterval(bufferCheckInterval);
+                                    tcpClient.resume();
+                                }
+                            }, DATA_CHANNEL_LOW_CHECK_INTERVAL);
+                        }
+
+                    });
+                    socket.on('end', () => {
+                        if (args['--verbose'])
+                            console.log(`TCP socket connection ended`)
+                        dataChannel.close();
+                    });
+                    socket.on('close', () => {
+                        if (args['--verbose'])
+                            console.log('TCP socket connection closed');
+                        dataChannel.close();
+                    });
+                    socket.on('error', (err) => {
+                        console.log('TCP socket error: ' + err)
+                    });
+                })
+                tcpServer.listen(args['<localPort>'], () => {
+                    serverPortConnected = true;
+                    console.log(`Peer client listening on port ${args['<localPort>']}`);
+                });
+
+                tcpServer.on('error', (err) => {
+                    console.log('TCP Error: ' + err)
+                    peer.destroy();
+                });               
+            } else {
+                // peer server was not able to setup a TCP port connection to the desired port
+                console.log(data);
+                peer.destroy();
+            }
         });
 
-        conn.send('Hello I am the client');
-
-        // OTHER DATA CHANNELS CREATED ON DEMAND BY CLIENT
-
-        // TODO
+        // Tell the server about which port we want to use on the server
+        conn.send(`p:${args['<serverPort>']}`);
     });
 });
 
@@ -128,6 +212,7 @@ peer.on('error', (err) => {
 
 peer.on('close', () => {
     console.log('PEER CLOSED');
+    process.exit();
 });
 
 process.on('SIGINT', () => {
@@ -136,212 +221,3 @@ process.on('SIGINT', () => {
     process.exit();
 });
 
-
-
-/*
-// P2P STUFF --------------------------------------------------------------------------------------
-
-function setupP2PStuff(){
-    const peer = new Peer({
-        initiator: true,
-        wrtc: wrtc,
-        trickle: false
-    });
-    let serverPortConnected = false;
-
-    peer.on('error', (err) => {
-        console.log('Peer error: ' + err);
-    });
-
-    peer.on('close', () => {
-        console.log('Connection closed');
-        process.exit();
-    });
-
-    peer.on('signal', (signalData) => {
-        if (args['--verbose'])
-            console.log('Signal generated');
-
-        const signalStr = JSON.stringify(signalData);
-
-        if (args['--manual-messaging']){
-            console.log('Copy the following SDP string and paste it in the server');
-            console.log(signalString);
-        } else {
-            if (args['--print-sdp-strings'])
-                printSDP(signalStr)
-
-            // Initiate the message
-            messenger.sendMessage(signalStr, (res) => {
-                if (args['--verbose'])
-                    console.log('Signal string sent to server')}
-            );
-        }
-    });
-
-    peer.on('connect', () => {
-        if (args['--verbose'])
-            console.log('Peer connected');
-
-        // Tell the server to connect to a TCP port on the server's local machine
-        peer.send(`p:${args['<serverPort>']}`);
-    });
-
-    peer.on('data', (chunk) => {
-        if (serverPortConnected) return
-
-        chunk = '' + chunk;
-        if (chunk === 'ok'){
-            // The server tells us that it has connected to the port that the user requested and is
-            // ready to forward the p2p data stream to it
-            console.log(`Peer server listening on port ${args['<serverPort>']}`);
-
-            const tcpServer = new net.createServer((socket) => {
-                if (args['--verbose'])
-                    console.log(`Something connected to local port ${args['<localPort>']}`);
-
-                // Create a new data channel on the peer with a random name
-                const dataChannel = peer._pc.createDataChannel('custom_' + randomBytes(20).toString('hex'))
-                dataChannel.binaryType = 'arraybuffer';
-                dataChannel.onmessage = (event) => {
-                    const chunk = Buffer.from(event.data);
-                    socket.write(chunk);
-                }
-                dataChannel.onclose = () => {
-                    if (args['--verbose'])
-                        console.log('data channel closed');
-                }
-                dataChannel.onerror = (err) => {
-                    console.log('data channel error: ' + err);
-                }
-
-                // Setup the socket to the use the data from the p2p data channel
-                socket.setKeepAlive(true);
-                socket.on('data', (chunk) => {
-                    // Protect the data channel buffer from getting filled up
-                    if (dataChannel.bufferedAmount < MAX_DATA_CHANNEL_BUFFER_SIZE){
-                        try {
-                            dataChannel.send(chunk);
-                        } catch(e) {
-                            console.log(e);
-                            tcpClient.end();
-                        }
-                    } else {
-                        tcpClient.pause();
-
-                        // Check the bufferedAmount until it goes acceptably low
-                        const bufferCheckInterval = setInterval(() => {
-                            if (dataChannel.bufferedAmount === 0){
-                                try {
-                                    dataChannel.send(chunk);
-                                } catch(e) {
-                                    console.log(e);
-                                    tcpClient.end();
-                                }
-                                clearInterval(bufferCheckInterval);
-                                tcpClient.resume();
-                            }
-                        }, DATA_CHANNEL_LOW_CHECK_INTERVAL);
-                    }
-
-                });
-                socket.on('end', () => {
-                    if (args['--verbose'])
-                        console.log(`TCP socket connection ended`)
-                    dataChannel.close();
-                });
-                socket.on('close', () => {
-                    if (args['--verbose'])
-                        console.log('TCP socket connection closed');
-                    dataChannel.close();
-                });
-                socket.on('error', (err) => {
-                    console.log('TCP socket error: ' + err)
-                });
-            })
-            tcpServer.listen(args['<localPort>'], () => {
-                serverPortConnected = true;
-                console.log(`Peer client listening on port ${args['<localPort>']}`);
-            });
-
-            tcpServer.on('error', (err) => {
-                console.log('TCP Error: ' + err)
-                peer.destroy();
-            });
-        } else {
-            // peer server was not able to setup a TCP port connection to the desired port
-            console.log(chunk);
-            peer.destroy();
-        }
-    });
-
-    return peer;
-}
-
-// MAIN -------------------------------------------------------------------------------------------
-
-if (args['--list-available-messengers']) {
-    console.log('Available messengers');
-
-    fs.readdirSync(MESSENGER_DIRECTORY).forEach(fileName => {
-        console.log(`    ${fileName.replace(/.js$/, '')}`);
-    })
-
-    process.exit();
-}
-
-if (args['--sdp-messenger']) {
-    const messengerScript = `${MESSENGER_DIRECTORY}/${args['--sdp-messenger']}.js`
-    if (!fs.existsSync(messengerScript)) {
-        console.log(`Messenger not found at ${messengerScript}`);
-        process.exit();
-    }
-
-    // Add a timestamp to the front of all console logs
-    require('log-timestamp');
-
-    // Import messengerScript
-    messenger = require(messengerScript);
-    if (!messenger) {
-        console.log(`Could not load module: ${messengerScript}`);
-        process.exit();
-    }
-
-    // Setup the messenger module
-    console.log(`Using messenger module: ${messengerScript}`);
-    messenger.init(config);
-    messenger.startListening(
-        () => {
-            if (args['--verbose'])
-                console.log('Messenger ready');
-            peerClient = setupP2PStuff()
-        },
-        (msg) => {
-            // If the peer client is already connected then do nothing
-            if (!peerClient || peerClient.connected)
-                return;
-
-            if (args['--verbose'])
-                console.log('Discord message received from server');
-
-            if (args['--print-sdp-strings'])
-                printSDP(msg);
-
-            peerClient.signal(JSON.parse(msg));
-        }
-    );
-}
-
-process.on('SIGINT', () => {
-    if (args['--verbose'])
-        console.log('Ctrl+c: EXITING');
-
-    if (!peerClient)
-        peerClient.destroy();
-
-    if (!messenger)
-        messenger.stopListening();
-
-    process.exit();
-});
-*/
